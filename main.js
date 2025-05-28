4c282c1c98447348a1687381040457f87fa737de
yannick biot <yannickpro64@gmail.com>
2025-05-28 15:42:45 +0200

    listener du bouton d'exploration, et composant country

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 // API
 import { getCountries, getCountry } from "./public/services/api.js";
+import { constructCountryPage } from "./public/components/country.js";
+/////////////
+//  Main
+////////////
+// On écoute le boutton d'exploration
+document.getElementById("btn_input").addEventListener("click", () => {
+  const country = document.getElementById("input").value;
+  const countryInfo = getCountry(country)
+  constructCountryPage(countryInfo)
+});
+
