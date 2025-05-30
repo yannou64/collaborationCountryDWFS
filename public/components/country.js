c1bc68365595f896631a36741c2d6379874b8bed
LaureLavie <laure.lavie@gmail.com>
2025-05-30 09:09:40 +0200

    css

--- public/components/country.js
+++ public/components/country.js
@@ -1,3 +1,16 @@
 export function constructCountryPage() {
-    
+  const section = document.getElementById("country");
+  section.innerHTML = `
+          <h2>${country.name}</h2>
+          <img src="${country.flags.svg}" alt="Drapeau de ${country.name}" width="150" />
+      
+          <button id="addToFavorites">Ajouter aux favoris</button>
+        `;
 }
+constructCountryPage();
+
+// Ajouter aux favoris
+document.getElementById("addToFavorites").addEventListener("click", () => {
+  addFavorite({ id: country.ccn3, name: country.name });
+  alert(`${country.name} ajouté aux favoris.`);
+});
